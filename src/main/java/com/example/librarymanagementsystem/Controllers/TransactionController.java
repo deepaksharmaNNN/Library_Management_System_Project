87ecc8f8ba52a05dbde577eda2d6697af038632e
Deepak Sharma <dsharma2828@gmail.com>
2024-01-21 12:41:47 +0530

    created Custom Exception package and classes

--- src/main/java/com/example/librarymanagementsystem/Controllers/TransactionController.java
+++ src/main/java/com/example/librarymanagementsystem/Controllers/TransactionController.java
@@ -2,6 +2,9 @@ package com.example.librarymanagementsystem.Controllers;
 
 import com.example.librarymanagementsystem.Services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +17,12 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
     @PostMapping("issueBook")
-    public String issueBook(@RequestParam("bookId") Integer bookId, @RequestParam("cardId") Integer cardId){
+    public ResponseEntity<String> issueBook(@RequestParam("bookId") Integer bookId, @RequestParam("cardId") Integer cardId){
         try {
-
+            String result = transactionService.issueBook(bookId, cardId);
+            return new ResponseEntity<>(result, HttpStatus.CREATED);
         }catch (Exception e){
-
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_GATEWAY);
         }
     }
 }
