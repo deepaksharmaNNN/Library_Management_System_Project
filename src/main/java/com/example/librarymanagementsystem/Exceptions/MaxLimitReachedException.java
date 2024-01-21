886a4e1e75b42f76e5337f165f03f095bb8a9857
Deepak Sharma <dsharma2828@gmail.com>
2024-01-21 12:46:43 +0530

    added more Custom Exception

---
+++ src/main/java/com/example/librarymanagementsystem/Exceptions/MaxLimitReachedException.java
@@ -0,0 +1,7 @@
+package com.example.librarymanagementsystem.Exceptions;
+
+public class MaxLimitReachedException extends Exception{
+    public MaxLimitReachedException(String message) {
+        super(message);
+    }
+}
