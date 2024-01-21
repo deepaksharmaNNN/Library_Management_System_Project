966d7cb495b353262717dc60c17164336629d240
Deepak Sharma <dsharma2828@gmail.com>
2024-01-21 15:56:02 +0530

    Completed email config for author and it works

--- src/main/java/com/example/librarymanagementsystem/Services/AuthorService.java
+++ src/main/java/com/example/librarymanagementsystem/Services/AuthorService.java
@@ -4,15 +4,25 @@ import com.example.librarymanagementsystem.Entities.Author;
 import com.example.librarymanagementsystem.Repository.AuthorRepository;
 import com.example.librarymanagementsystem.RequestDtos.AddAuthorRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
+    @Autowired
+    private JavaMailSender javaMailSender;
     public String addAuthor(AddAuthorRequest addAuthorRequest){
         Author authorEntity = new Author(addAuthorRequest.getAuthorName(), addAuthorRequest.getAuthorAge(), addAuthorRequest.getEmailId());
         Author newAuthor = authorRepository.save(authorEntity);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("Hi "+newAuthor.getAuthorName()+" !");
+        message.setFrom("yonkai28@gmail.com");
+        message.setTo(newAuthor.getEmailId());
+        message.setText("Your have been Successfully registered on our portal !" +" Looking forward for you to use our services");
+        javaMailSender.send(message);
         return "Author with authorId "+newAuthor.getAuthorId()+" has been added to Db";
     }
 }
